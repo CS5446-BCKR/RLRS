aafe99a9c5190d6d6d7f12b0646daca61e0f5dd7
Dang-Khoa <letan.dangkhoa@gmail.com>
2024-03-02 15:32:26 +0800

    train the online Actor network

--- rlrs/nets/actor.py
+++ rlrs/nets/actor.py
@@ -1,7 +1,12 @@
 from torch import nn
+from torch.optim import lr_scheduler, Adam
 
 
 class ActorModel(nn.Module):
+    """
+    Actor Model, or the policy network creates *actions* (not real action through),
+    it needs to multiply with item embeddings to get the ranked score for recommended items.
+    """
     def __init__(self, input_dim: int, hidden_dim: int, output_dim: int):
         super(ActorModel, self).__init__()
         self.layers = [
@@ -19,9 +24,24 @@ class ActorModel(nn.Module):
 
 
 class Actor:
-    def __init__(self, input_dim, hidden_dim, output_dim, tau):
+    def __init__(self, input_dim, hidden_dim, output_dim, tau, lr, step_size):
         self.online_network = ActorModel(input_dim, hidden_dim, output_dim)
         self.target = ActorModel(input_dim, hidden_dim, output_dim)
         self.tau = tau
+        self.lr = lr
+        self.step_size = step_size
+        # hard code optimizer here
+        self.optim = Adam(self.online_network.parameters(), lr=self.lr)
+        self.schedular = lr_scheduler.StepLR(self.optim, step_size=self.step_size)
 
-    def update(self): ...
+    def update_target(self): ...
+
+    def fit_online_network(self, states, gradients):
+        """
+        Fit the source network via states and gradients
+        """
+        self.online_network.train()
+        self.optim.zero_grad()
+        outputs = self.online_network(states)
+        outputs.backward(gradients)
+        self.scheduler.step()
