87049676fd94743b9002559cb3d4c9af8250f7e1
Dang-Khoa Le Tan <letan.dangkhoa@gmail.com>
2024-03-06 01:19:06 +0800

    PriorityExperienceReplay

---
+++ rlrs/replay/replay_buffer.py
@@ -0,0 +1,88 @@
+import numpy as np
+from trees import SumTree, MinTree
+import random
+
+
+class PriorityExperienceReplay:
+    def __init__(self, buffer_size, embedding_dim):
+        self.buffer_size = buffer_size
+        self.embedding_dim = embedding_dim
+        self.crt_idx = 0
+        self.is_full = False
+
+        # setup buffers
+        self.states = np.zeros(
+            (buffer_size, 3 * embedding_dim), dtype=np.float32)
+        self.actions = np.zeros((buffer_size, embedding_dim), dtype=np.float32)
+        self.rewards = np.zeros((buffer_size,), dtype=np.float32)
+        self.next_states = np.zeros(
+            (buffer_size, 3 * embedding_dim), dtype=np.float32)
+        self.dones = np.zeros((buffer_size,), dtype=np.bool)
+
+        self.sum_tree = SumTree(buffer_size)
+        self.min_tree = MinTree(buffer_size)
+
+        self.max_priority = 1.0
+        self.alpha = 0.6
+        self.beta = 0.4
+        self.beta_constant = 1e-5
+
+    def append(self, state, action, reward, next_state, done):
+        self.states[self.crt_idx] = state
+        self.actions[self.crt_idx] = action
+        self.rewards[self.crt_idx] = reward
+        self.next_states[self.crt_idx] = next_state
+        self.dones[self.crt_idx] = done
+
+        self.sum_tree.add_data(self.max_priority**self.alpha)
+        self.min_tree.add_data(self.max_priority**self.alpha)
+
+        self.crt_idx = (self.crt_idx + 1) % self.buffer_size
+        if self.crt_idx == 0:
+            self.is_full = True
+
+    def update_priority(self, priority, index):
+        self.sum_tree.update_priority(priority**self.alpha, index)
+        self.min_tree.update_priority(priority**self.alpha, index)
+        self.update_max_priority(priority)
+
+    def update_max_priority(self, priority):
+        self.max_priority = max(self.max_priority, priority)
+
+    def sample(self, batch_size):
+        rd_idx = []
+        weight_batch = []
+        index_batch = []
+        sum_priority = self.sum_tree.sum_all_priority()
+
+        N = self.buffer_size if self.is_full else self.crt_idx
+        min_priority = self.min_tree.min_priority() / sum_priority
+        max_weight = (N * min_priority) ** (-self.beta)
+
+        segment_size = sum_priority / batch_size
+
+        for i in range(batch_size):
+            min_seg = segment_size * i
+            max_seg = segment_size * (i + 1)
+
+            j = random.uniform(min_seg, max_seg)
+            priority, tree_index, buffer_index = self.sum_tree.search(j)
+            rd_idx.append(buffer_index)
+
+            pi = priority / sum_priority
+            wi = (pi * N) ** (-self.beta) / max_weight
+
+            weight_batch.append(wi)
+            index_batch.append(tree_index)
+
+        self.beta = min(1.0, self.beta + self.beta_constant)
+
+        return (
+            self.states[rd_idx],
+            self.actions[rd_idx],
+            self.rewards[rd_idx],
+            self.next_states[rd_idx],
+            self.dones[rd_idx],
+            np.array(weight_batch),
+            index_batch,
+        )
