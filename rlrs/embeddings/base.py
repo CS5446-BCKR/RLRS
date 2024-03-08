d4495eb8a4d8fe8f7cea6c9ea40197437b0f4e91
Dang-Khoa Le Tan <letan.dangkhoa@gmail.com>
2024-03-09 01:05:48 +0800

    add dummy embedding

--- rlrs/embeddings/base.py
+++ rlrs/embeddings/base.py
@@ -1,6 +1,22 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class EmbeddingBase(ABC):
     @abstractmethod
     def fit(self, data): ...
+
+
+class DummyEmbedding(EmbeddingBase):
+    """
+    Generate random matrix of size NxD
+    where N is the number of data point, D is the embedding dimension.
+    """
+
+    def __init__(self, dim):
+        self.dim = dim
+
+    def fit(self, data):
+        N = data.shape[0]
+        return np.random.rand(N, self.dim)
