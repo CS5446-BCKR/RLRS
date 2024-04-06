7b6f521f1dfec432484e3c81073a5c6344e0005d
dangkhoasdc <letan.dangkhoa@gmail.com>
2024-04-06 11:42:54 +0800

    add loading pretrained embeddings

--- rlrs/embeddings/base.py
+++ rlrs/embeddings/base.py
@@ -5,8 +5,10 @@ TODO: Return Torch tensor
 from abc import ABC, abstractmethod
 from functools import cached_property
 
+import numpy as np
 import pandas as pd
 import torch
+from path import Path
 
 
 class EmbeddingBase(ABC):
@@ -17,23 +19,9 @@ class EmbeddingBase(ABC):
     def __getitem__(self, index) -> torch.Tensor: ...
 
 
-class DummyEmbedding(EmbeddingBase):
-    """
-    Generate random matrix of size NxD
-    where N is the number of data point, D is the embedding dimension.
-    """
-
-    def __init__(self, dim):
-        self.dim = dim
-        self.embeddings = None
-
-    def fit(self, data):
-        """
-        Argument:
-            data: DataFrame with index
-        """
-        self.embs = pd.DataFrame(index=data.index)
-        self.embs["emb"] = self.embs.index.map(lambda i: torch.randn(self.dim))
+class AccessWithIndexMixin:
+    def __init__(self):
+        self.embs = None
 
     def __getitem__(self, index) -> torch.Tensor:
         assert self.embs is not None
@@ -44,6 +32,45 @@ class DummyEmbedding(EmbeddingBase):
         if not embs:
             return None
         return torch.stack(embs)
+
+
+class PretrainedEmbedding(AccessWithIndexMixin, EmbeddingBase):
+    def __init__(self, data: torch.Tensor, index):
+        super(PretrainedEmbedding).__init__()
+        self.embs = pd.DataFrame(index=index)
+        index2iloc = {ind: i for i, ind in enumerate(index)}
+        self.embs["emb"] = self.embs.index.map(lambda ind: data[index2iloc[ind]])
+
+    def fit(self, data): ...
+
+    @classmethod
+    def from_h5(clz, file_path: Path, object_name: str):
+        import h5py
+
+        with h5py.File(file_path, "r") as f:
+            data = np.asarray(f[f"{object_name}_embedding"])
+            index = list(f[f"{object_name}_index"])
+            data = torch.from_numpy(data)
+            return clz(data, index)
+
+
+class DummyEmbedding(AccessWithIndexMixin, EmbeddingBase):
+    """
+    Generate random matrix of size NxD
+    where N is the number of data point, D is the embedding dimension.
+    """
+
+    def __init__(self, dim):
+        super(DummyEmbedding).__init__()
+        self.dim = dim
+
+    def fit(self, data):
+        """
+        Argument:
+            data: DataFrame with index
+        """
+        self.embs = pd.DataFrame(index=data.index)
+        self.embs["emb"] = self.embs.index.map(lambda i: torch.randn(self.dim))
 
     @cached_property
     def all(self):
--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import torch
 
-from rlrs.embeddings.base import DummyEmbedding
+from rlrs.embeddings.base import DummyEmbedding, PretrainedEmbedding
 
 
 def test_dummy_embedding():
@@ -21,3 +21,14 @@ def test_dummy_embedding():
     assert all.size() == (3, 5)
 
     emb = embeds[[]]
+
+
+def test_pretrained_embedding():
+    PATH = "data/test_data/embed_out/embeddings.h5"
+    user_embs = PretrainedEmbedding.from_h5(PATH, "user")
+    all = user_embs[[5, 6, 7, 8, 9, 10]]
+    assert all.shape == (6, 5)
+
+    item_embs = PretrainedEmbedding.from_h5(PATH, "item")
+    all = item_embs[[10, 11, 12, 13, 14, 15]]
+    assert all.shape == (6, 5)
